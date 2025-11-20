3838652140522ea095a552c7fcf32e9eee5cada6
Yuan He <yuanhe.cs.ai@gmail.com>
2025-11-20 01:44:27 +0000

    chore add coding agent example

--- examples/strands_agent/coding_agent.py
+++ examples/strands_agent/coding_agent.py
@@ -2,17 +2,15 @@
 Minimal Strands-Agents-based coding agent that uses the `CodeSandbox` to execute code in a sandbox environment.
 """
 
-
 from code_sandbox import CodeSandbox
 from strands import Agent
 from strands.models import BedrockModel
-
 
 # The default model is `claude-4.0`
 model = BedrockModel()
 
 code_sandbox = CodeSandbox(
-    workdir="/workspace/examples/strands_agent/code_sandbox",
+    workdir="/tmp/code_sandbox",
     language="python",
     execution_timeout=300,
 )
@@ -21,7 +19,8 @@ code_sandbox.session.install(["numpy"])
 if __name__ == "__main__":
     agent = Agent(model=model, tools=[code_sandbox.execute_code])
     agent(
-        "Write a simple numpy program to calculate the eigen-values of a matrix and execute it on a random matrix."
+        "Write a simple numpy program to calculate the eigen-values of a matrix and execute it on a "
+        "simple matrix [[1, 2, 3], [4, 5, 6], [7, 8, 9]]."
     )
     code_sandbox.close_session()
     print("Code sandbox closed")
