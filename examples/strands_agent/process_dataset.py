6fca7c6dea647eca17b07c38f039051bbcbbd02b
Yuan He <yuanhe.cs.ai@gmail.com>
2025-11-25 02:26:55 +0000

    chore: update aime

--- examples/strands_agent/process_dataset.py
+++ examples/strands_agent/process_dataset.py
@@ -6,15 +6,29 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(root_dir, "data")
 
 
-def transform(example):
+def transform_dapo_math(example):
     return {
         "prompt": example["prompt"][0]["content"] if example["prompt"] else None,
         "label": example["reward_model"]["ground_truth"],
     }
 
+def transform_aime(example):
+    return {
+        "prompt": example["prompt"][0]["content"] if example["prompt"] else None,
+        "label": example["label"],
+    }
+
 
 if __name__ == "__main__":
+
+    # training dataset is from DAPO-Math-17k
     dataset = load_dataset("BytedTsinghua-SIA/DAPO-Math-17k", split="train")
-    dataset = dataset.map(transform, remove_columns=dataset.column_names)
+    dataset = dataset.map(transform_dapo_math, remove_columns=dataset.column_names)
     os.makedirs(data_dir, exist_ok=True)
-    dataset.to_json(os.path.join(data_dir, "dapo_math_17k_cleaned.jsonl"), orient="records", lines=True)
+    dataset.to_json(os.path.join(data_dir, "dapo_math_17k.jsonl"), orient="records", lines=True)
+
+    # evaluation dataset is from AIME-2024
+    dataset = load_dataset("zhuzilin/aime-2024", split="train")
+    dataset = dataset.map(transform_aime, remove_columns=dataset.column_names)
+    os.makedirs(data_dir, exist_ok=True)
+    dataset.to_json(os.path.join(data_dir, "aime_2024.jsonl"), orient="records", lines=True)
