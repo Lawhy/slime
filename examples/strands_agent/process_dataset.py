a5040f1160c744c820397a967ff7f2df603924b0
Yuan He <yuanhe.cs.ai@gmail.com>
2025-11-25 04:51:26 +0000

    chore: udpate dir

--- examples/strands_agent/process_dataset.py
+++ examples/strands_agent/process_dataset.py
@@ -2,8 +2,7 @@ import os
 
 from datasets import load_dataset
 
-root_dir = os.path.dirname(os.path.abspath(__file__))
-data_dir = os.path.join(root_dir, "data")
+DATA_DIR = "/root/data"
 
 
 def transform_dapo_math(example):
@@ -21,14 +20,14 @@ def transform_aime(example):
 
 if __name__ == "__main__":
 
+    os.makedirs(DATA_DIR, exist_ok=True)
+
     # training dataset is from DAPO-Math-17k
     dataset = load_dataset("BytedTsinghua-SIA/DAPO-Math-17k", split="train")
     dataset = dataset.map(transform_dapo_math, remove_columns=dataset.column_names)
-    os.makedirs(data_dir, exist_ok=True)
-    dataset.to_json(os.path.join(data_dir, "dapo_math_17k.jsonl"), orient="records", lines=True)
+    dataset.to_json(os.path.join(DATA_DIR, "dapo_math_17k.jsonl"), orient="records", lines=True)
 
     # evaluation dataset is from AIME-2024
     dataset = load_dataset("zhuzilin/aime-2024", split="train")
     dataset = dataset.map(transform_aime, remove_columns=dataset.column_names)
-    os.makedirs(data_dir, exist_ok=True)
-    dataset.to_json(os.path.join(data_dir, "aime_2024.jsonl"), orient="records", lines=True)
+    dataset.to_json(os.path.join(DATA_DIR, "aime_2024.jsonl"), orient="records", lines=True)
